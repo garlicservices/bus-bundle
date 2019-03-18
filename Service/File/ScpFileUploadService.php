1990211c5f34f0312d1a81246f78683a9305fc7d
alexandr <a.odarych@hubber.pro>
2019-03-18 18:17:01 +0200

    Fix known hosts issue

--- Service/File/ScpFileUploadService.php
+++ Service/File/ScpFileUploadService.php
@@ -49,7 +49,8 @@ class ScpFileUploadService
     {
         $filePath = $this->uploadDir.md5(time().$metadata['origin_name']).'.'.$metadata['extension'];
         try {
-            exec('sshpass -p "'.getenv('SCP_PASSWORD').'" scp -o StrictHostKeyChecking=no '.getenv('SCP_USERNAME').'@'.$metadata['host_url'].':'
+            exec('sshpass -p "'.getenv('SCP_PASSWORD').'" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null '
+                .getenv('SCP_USERNAME').'@'.$metadata['host_url'].':'
                 .$metadata['path'].' '.$filePath.' 2>&1', $output, $returnVar);
         } catch (\Exception $e) {
             throw new \HttpException($e->getMessage(), $e->getCode());
